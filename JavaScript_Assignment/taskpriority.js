072ff65eecef317d173f73e2b8e9476d7e3caa42
codegirlMaya1 <ebonyslove209@gmail.com>
2024-08-30 17:48:40 -0600

    Add files via upload

---
+++ JavaScript_Assignment/taskpriority.js
@@ -0,0 +1,72 @@
+
+
+//Making Pizza ingredients
+
+
+let num1=1;
+let num2=2;
+let num3=3;
+let num4=4;
+let num5=5
+let num6=6;
+let num7=7;
+let num8=8;
+let num9=9;
+let num0=0;
+
+//Chicken Pineapple Pizza
+let dough=num1
+let Marina=num1
+let GrilledChickenPack=num2
+let cheese=num2
+let oliveOil=num1
+let Pineapple=num2
+// Pizza Ingredient list
+
+let ChickenPizza=dough+Marina+GrilledChickenPack+cheese+oliveOil+Pineapple;
+
+let CheesePizza=ChickenPizza-Pineapple-GrilledChickenPack+cheese;
+console.log("CheesePizza:", CheesePizza);
+console.log("ChickenPizza:", ChickenPizza);
+console.log("Sum:", ChickenPizza);
+console.log("Difference:", ChickenPizza, CheesePizza);
+console.log("Product:", CheesePizza);
+console.log("Quotient:", ChickenPizza);
+console.log("Is dough equal to Marina?", dough==Marinara);
+console.log("Is num1 greater than num2?", Pineapple>= Marina);
+console.log("Is dough equal to Marina?", dough===Marinara);
+let isPositive = X = 12;
+let isEven = 24;
+console.log("Are both numbers positive?", isPositive);
+console.log("Is at least one number even?", isEven);
+
+studentName = "Joe";
+studentAge = 17;
+studentGrade = 95;
+isPresent = true;
+
+studentName1 = "John";
+studentAge1 = 17;
+studentGrade1 = 70;
+isPresent1 = false;
+
+studentName +="is an good student";
+studentAge +="is the student's age";
+studentGrade +="is a good grade";
+isPresent+="= the student's is present today"
+ console.log("-"+studentName)
+ console.log("-"+studentAge)
+ console.log("-"+studentGrade)
+ console.log("-" + studentGrade)
+ console.log("-"+isPresent)
+
+studentName1 +="is an average student";
+studentAge1 +="is the student's age";
+studentGrade1 +="is an average grade";
+isPresent+="= the student's is not present today. John is absent."
+console.log("-"+studentName1)
+console.log("-"+studentAge1)
+console.log("-"+studentGrade1)
+console.log("-" + studentGrade1)
+console.log("-"+isPresent1)
+
